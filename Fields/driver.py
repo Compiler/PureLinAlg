fec6b9f3752f7d4b239dfa957d83f812456a1339
Luke <LukeRocheDev@gmail.com>
2021-12-09 18:05:28 -0500

    more cmplx transformations

--- Fields/driver.py
+++ Fields/driver.py
@@ -1,10 +1,13 @@
 from plotting import plot
 from image import file2image
+from math import sqrt
 
 def Identity(S): return {z for z in S}
 def Translate(x, y, S): return {(x + y*1j) + z for z in S}
 def Scale(sz, S): return {z * sz for z in S}
-def Rotate(d, S): return {-1j * z for z in S}
+def Rotate(d, S): return {-1j**d * z for z in S}
+
+def Center(S): return {z - 100 + -len(S)/100*1j for z in S}
 S = {2+2j, 3+2j, 1.75 + 1j, 2+1j, 2.25 + 1j, 2.5+1j, 2.75 + 1j, 3+1j, 3.25 + 1j}
 
 #plot({((1+2j+z) * 0.5) * -1j for z in S}, 4)
@@ -13,4 +16,6 @@ S = {2+2j, 3+2j, 1.75 + 1j, 2+1j, 2.25 + 1j, 2.5+1j, 2.75 + 1j, 3+1j, 3.25 + 1j}
 
 S = file2image('img01.png')
 print(S[0][0])
-plot({ y * 1j + x for y in range(len(S)) for x in range(len(S[0])) if S[len(S) - y - 1][x][1] < 120}, len(S))
+#plot({ y * 1j + x for y in range(len(S)) for x in range(len(S[0])) if S[len(S) - y - 1][x][1] < 120}, len(S))
+width = len(S); height = len(S[0])
+plot({(y * 1j + x) - width / 2 - height / 2 * 1j for y in range(len(S)) for x in range(len(S[len(S) - y - 1])) if S[len(S) - y - 1][x][1] < 120}, len(S))
